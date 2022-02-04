663c81296bb85af2ec05979d2ebe12722a1e8888
Bruno Brum <brunobrum@gmail.com>
2022-02-03 19:07:33 -0500

    Criação das classes Categoria e Lanche

---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,21 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferdo { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //definindo o relacionamento entre Lanche e Categoria. Esses campos não são criados na
+        //tabela.
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
+    }
+}
